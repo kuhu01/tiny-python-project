1de6ebd5815af63782cb8b6a5145571847b26252
kuhu01 <110257060+kuhu01@users.noreply.github.com>
2024-04-23 18:40:46 +0530

    Add files via upload

---
+++ flower.py
@@ -0,0 +1,23 @@
+import turtle
+t= turtle.Turtle()
+s= turtle.Screen()
+s.bgcolor("black")
+t.pencolor("pink")
+t.speed(0)
+for i in range(150):
+    t.circle(190-i,90)
+    t.lt(90)
+    t.circle(190-i,90)
+    t.lt(18)
+    
+s.mainloop()
+
+
+
+#for i in range(100):
+ #   t.circle(150-i,90)
+  #  t.lt(70)
+   # t.circle(150-i,90)
+   # t.lt(7)
+    
+#s.mainloop()
